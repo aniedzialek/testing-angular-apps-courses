683716fe16f850d8987973fc80823bfbfb6d7cd5
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2018-03-16 19:21:43 +0100

    Basic forms tests

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,25 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { TodoFormComponent } from './todo-form.component';
+import {FormBuilder} from '@angular/forms';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent;
+  let formBuilder: FormBuilder;
 
   beforeEach(() => {
-
+    formBuilder = new FormBuilder();
+    component = new TodoFormComponent(formBuilder);
   });
 
-  it('', () => {
+  it('creates form with 2 controls', () => {
+    expect(component.form.contains('name')).toBe(true);
+    expect(component.form.contains('email')).toBe(true);
   });
 
-  it('', () => {
+  it('name control is required', () => {
+    const nameControl = component.form.get('name');
+
+    nameControl.setValue('');
+
+    expect(nameControl.valid).toBe(false);
   });
 });
