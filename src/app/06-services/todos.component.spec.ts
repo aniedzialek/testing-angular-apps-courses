e7f3be455d97c666d9a7d1dee45293a697900b2f
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2018-03-16 20:02:30 +0100

    Test for component with a service

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,34 @@
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('sets todos property with items returned from server', () => {
+    // Changing implementation of getTodos() with Jasmine's spyOn.
+    // With use of spyOn you get the control over a method in a class
+
+    const todos = [1, 2, 3];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([ todos ]);  // or:
+      // return Observable.from( [ [
+      //   {id: 1, title: 'a'},
+      //   {id: 2, title: 'b'},
+      //   {id: 3, title: 'c'},
+      // ] ]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos).toBe(todos);
   });
 });
--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -1,18 +1,19 @@
+import { TodoService } from './todo.service';
+import {OnInit} from '@angular/core';
 
-import { TodoService } from './todo.service'
-
-export class TodosComponent { 
+export class TodosComponent implements OnInit {
   todos: any[] = [];
-  message; 
+  message;
 
   constructor(private service: TodoService) {}
 
-  ngOnInit() { 
-    this.service.getTodos().subscribe(t => this.todos = t);
+  ngOnInit() {
+    this.service.getTodos()
+      .subscribe(t => this.todos = t);
   }
 
-  add() { 
-    var newTodo = { title: '... ' };
+  add() {
+    const newTodo = { title: '... ' };
     this.service.add(newTodo).subscribe(
       t => this.todos.push(t),
       err => this.message = err);
@@ -21,5 +22,5 @@ export class TodosComponent {
   delete(id) {
     if (confirm('Are you sure?'))
       this.service.delete(id).subscribe();
-  }  
+  }
 }
